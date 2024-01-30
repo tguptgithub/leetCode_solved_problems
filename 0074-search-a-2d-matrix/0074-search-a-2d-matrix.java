3c8ad2f5c4c426b8e53aed33dc5c7973affff5ba
tguptgithub <121037070+tguptgithub@users.noreply.github.com>
2024-01-30 13:53:24 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (31.82%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -13,19 +13,35 @@ class Solution {
         // return false;
         //Little optimised
         // Applying binary search in rowwise:
-        for(int i=0;i<n;i++){
-            int k=0; int j=m-1;
+        // for(int i=0;i<n;i++){
+        //     int k=0; int j=m-1;
+        //     while(k<=j){
+        //     int mid=(k+j)/2;
+        //     if(matrix[i][mid]==target){
+        //         return true;
+        //     }else if(matrix[i][mid]<target) {
+        //         k++;}else{
+        //             j--;
+        //         }
+        //     }
+        // }
+        // return false;
+        // column wise traversal
+          for(int i=0;i<m;i++){
+            int k=0; int j=n-1;
             while(k<=j){
             int mid=(k+j)/2;
-            if(matrix[i][mid]==target){
+            if(matrix[mid][i]==target){
                 return true;
-            }else if(matrix[i][mid]<target) {
+            }else if(matrix[mid][i]<target) {
                 k++;}else{
                     j--;
                 }
             }
         }
-        return false;    
+        return false; 
+        
+        
     
     }
 }
