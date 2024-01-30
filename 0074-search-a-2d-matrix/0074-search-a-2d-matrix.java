0dfdd78ee9ecae104358412155e640387668e532
tguptgithub <121037070+tguptgithub@users.noreply.github.com>
2024-01-30 09:29:02 +0530

    Time: 0 ms (100.00%), Space: 42 MB (41.82%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+     //bruteForce
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(matrix[i][j]==target){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+        //Little optimised
+        // Applying binary search in rowwise:
+        for(int i=0;i<n;i++){
+            int k=0; int j=m-1;
+            while(k<=j){
+            int mid=(k+j)/2;
+            if(matrix[i][mid]==target){
+                return true;
+            }else if(matrix[i][mid]<target) {
+                k++;}else{
+                    j--;
+                }
+            }
+        }
+        return false;    
+    
+    }
+}
