9a842fafb12e029c8e5ba71ec6752b27c75c307d
tguptgithub <121037070+tguptgithub@users.noreply.github.com>
2024-03-22 09:10:22 +0530

    Time: 1 ms (99.47%), Space: 56.7 MB (79.29%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,19 +1,17 @@
 class Solution {
 
     public int maxSubArray(int[] nums) {
-         int ans=nums[0];
-        int sum=nums[0];
-        int n=nums.length;
-        for(int i=1;i<n;i++){
+        int ans=nums[0]; //-2
+        int sum=nums[0];//-2
+        for(int i=1;i<nums.length;i++){
             if(sum<0){
-                sum=0;
+                sum=0; //0 i=3 ,4 sum=0;
             }
-            sum+=nums[i];
-            ans=Math.max(ans, sum);
+            sum=sum+nums[i]; // 0+1+-3 =-2 , 0+4=4, 4-1=3, 3+2=5, 5+1=6, 6-5=1, 1+4=5 
+            ans=Math.max(ans,sum); //1 , 4 , 5,6, 
+            
         }
         return ans;
-            }
         
-      
-    
+    }    
 }
